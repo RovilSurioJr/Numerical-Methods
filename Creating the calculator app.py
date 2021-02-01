c38ed8ff379b350a1ed7e9365d386c7598d29185
RovilSurioJr <70187981+RovilSurioJr@users.noreply.github.com>
2021-02-02 00:39:32 +0800

    Create Creating the calculator app.py

---
+++ Creating the calculator app.py
@@ -0,0 +1,11 @@
+'''
+Create a grade calculator that computes for the semestral grade of a course.
+Students could type their names, the name of the course, then their prelim,
+midterm, and final grade.
+The program should print the semestral grade in 2 decimal points and should
+display the following emojis depending on the situation:
+happy - when grade is greater than 70.00
+laughing - when grade is exactly 70.00
+sad - when grade is below 70.00
+'''
+happy, lol, sad = "\U0001F600","\U0001F923","\U0001F619"
